5a06e740d4c2193a27fd0e017953dd35443480ef
Ademola Israel <ogundanaademolaisrael@gmail.com>
2023-08-11 02:44:47 +0100

    bkapi-1 - define data schema

---
+++ src/models/schema/booking.mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const bookingSchema = mongoose.Schema({
+ placeId: {
+  type: String,
+  required : true,
+ },
+ userId:{
+  type: String,
+  required :true
+ },
+ placeTitle:{
+  type: true,
+  required: true
+ },
+ placeImage:{
+  type:String,
+  required: true
+ },
+ firstName:{
+  type: String,
+  required: true,
+ },
+ lastName:{
+  type: String,
+  required: true
+ },
+ guestNumber:{
+  type: Number,
+  required: true,
+  minlength:[1,'Please enter a valid value'],
+  maxlength:[5,"Guests can't exceed more than 5 people"]
+ },
+ bookedFrom:{
+  type:Date,
+  required: true,
+ },
+ bookedTo:{
+  type:Date,
+  required: true,
+ }
+});
+
+module.exports = mongoose.model('Booking', bookingSchema)
